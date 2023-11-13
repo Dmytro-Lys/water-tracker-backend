c72a0254411d587bc28b7954a40bf6a1d19ef766
Dmytro-Lys <d.lysenko122@gmail.com>
2023-11-13 17:21:10 +0200

    swagger-ui-express base config

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import multer from "multer";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json'  assert { type: "json" };
 import authRouter from "./routes/api/auth-router.js";
 import waterInputRouter from "./routes/api/waterInputRouter.js";
 
@@ -15,6 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use("/api/users", authRouter);
 app.use("/api/waterInputs", waterInputRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });

7079e0aeed1d2f9e94be1a1da4f9b0b0215ccdf0
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-11-20 18:40:38 +0200

    delete waterInputRouter

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,4 +4,3 @@ export { default as waterRateRouter } from "./waterRateRouter.js";
 export { default as waterRouter } from "./waterRouter.js";
 export { default as monthWaterRouter } from "./monthWaterRouter.js";
 export { default as todayWaterRouter } from "./todayWaterRouter.js";
-export { default as waterInputRouter } from "./waterInputRouter.js";

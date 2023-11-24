2f72f30d25831d04ab1913a0e0e53a78d41cc596
Dmytro-Lys <d.lysenko122@gmail.com>
2023-11-24 16:51:13 +0200

    fix Userschema validation (max, min)

--- models/User.js
+++ models/User.js
@@ -6,11 +6,10 @@ import { handleSaveError, runValidatorsAtUpdate } from "./hooks.js";
 import { addFieldMongoose, addFieldJoi } from "./validation-tools.js";
 
 const regExpPassword =  /^\S+$/
-
+//const regExpPassword = /(?=.*[0-9])(?=.*[!@#$%^&*()\-\_=+,<.>/?;:'\[\]{}|])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*()\-\_=+,<.>/?;:'\[\]{}|]+/,
+//const errorMessagePassword = "The password must contain at least 1 lowercase letter, 1 uppercase letter, 1 number and 1 special character",
 const userShemaValidation = {
    password: {
-      // regExp: /(?=.*[0-9])(?=.*[!@#$%^&*()\-\_=+,<.>/?;:'\[\]{}|])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*()\-\_=+,<.>/?;:'\[\]{}|]+/,
-      // errorMessage: "The password must contain at least 1 lowercase letter, 1 uppercase letter, 1 number and 1 special character",
       regExp: regExpPassword,
       errorMessage: "The password contains invalid characters",
       requiredErrorMessage: 'Set password for user'
@@ -38,9 +37,9 @@ const userShemaValidation = {
 const genderList = ["female", "male"]
 // Mongoose
 const userSchemaDB = new Schema({
-   password: { ...addFieldMongoose(userShemaValidation.password), minlength: 8 },
+   password: { ...addFieldMongoose(userShemaValidation.password), minlength: 8, maxlength: 64 },
    email: { ...addFieldMongoose(userShemaValidation.email), unique: true },
-   userName: { ...addFieldMongoose(userShemaValidation.userName), required: false, minlength: 3 },
+   userName: { ...addFieldMongoose(userShemaValidation.userName), required: false, maxlength: 32 },
    waterRate: {
       type: Number,
       min: 1,
@@ -82,7 +81,7 @@ const User = model("user", userSchemaDB);
 // Joi
 export const userSchemaAuth = Joi.object({
    password: addFieldJoi.call(Joi, userShemaValidation.password)
-         .min(8),
+         .min(8).max(64),
     email: addFieldJoi.call(Joi, userShemaValidation.email)
 }) 
 
@@ -97,10 +96,10 @@ export const userSchemaWaterRate = Joi.object({
 export const userSchemaAll = Joi.object({
    email: addFieldJoi.call(Joi, userShemaValidation.email, "",  false ),
    oldPassword: addFieldJoi.call(Joi, userShemaValidation.oldPassword, "",  false )
-         .min(8),
+         .min(8).max(64),
    newPassword: addFieldJoi.call(Joi, userShemaValidation.newPassword, "",  false )
-         .min(8),
-    userName: addFieldJoi.call(Joi, userShemaValidation.userName, "", false ),
+         .min(8).max(64),
+    userName: addFieldJoi.call(Joi, userShemaValidation.userName, "", false ).max(32),
     gender: Joi.string().valid(...genderList)
 })
 
